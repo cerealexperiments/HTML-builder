a618ebc5c4c6f89c1844e215f35824d21271b0aa
rin <prettysunshine468@gmail.com>
2022-11-08 01:23:46 +0600

    fix 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,13 +2,22 @@ const { stdin, stdout } = process;
 const fs = require("fs");
 const path = require("path");
 
+fs.writeFile(path.join(__dirname, "text.txt"), "", (err) => {
+  if (err) throw err;
+})
+
 stdout.write("Hey! Type something:\n");
 stdin.on("data", (data) => {
-  const input = data.toString();
-  stdout.write(`Your input: ${input}`);
-  fs.writeFile(path.join(__dirname, "text.txt"), input, (error) => {
-    if (error) throw error;
-    console.log("file has been created");
+  const input = data.toString().trimEnd();
+  if(input === "exit") {
     process.exit();
-  });
+  } else {
+    fs.writeFile(path.join(__dirname, "text.txt"), input, (error) => {
+      if (error) throw error;
+      process.exit();
+    });
+  }
 });
+process.on("exit", ()  => {
+  console.log("Goodbye!");
+})
