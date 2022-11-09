f84397736a928dbb9b5da93f8b43730bad9e67c1
rin <prettysunshine468@gmail.com>
2022-11-09 12:47:34 +0600

    fix 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,29 +1,54 @@
 const fs = require("fs");
 const path = require("path");
 
-const inputPath = path.join(__dirname, "files");
-const outputPath = path.join(__dirname, "files-copy");
-fs.mkdir(outputPath, (err) => {
-  if (err) {
-    if(err.errno === -17) {
-      console.log("folder already exists, skipping this step")
-    } else {
-      throw err;
-    }
-  } else {
-    console.log("files-copy directory created");
-  }
-})
-
-fs.readdir(inputPath, (err, files) => {
-  if (err) throw err;
-  files.forEach(fileName => {
-    let inputFilePath = path.join(inputPath, fileName);
-    let outputFilePath = path.join(outputPath, fileName);
-    fs.copyFile(inputFilePath, outputFilePath, (err) => {
-      if (err) throw err;
-      console.log(`${fileName} copied to ${outputFilePath}`)
+const deleteFile = (filePath) => {
+  return new Promise(resolve => {
+    fs.unlink(filePath, (err) => {
+      if(err) throw err;
+      resolve();
     })
   })
-})
+}
+
+const makeDirectory = (filePath) => {
+  return new Promise(resolve => {
+    fs.mkdir(filePath,  (err) => {
+      if (err) {
+        if(err.errno === -17) {
+          console.log("folder already exists, deleting the old contents");
+          fs.readdir(filePath,  (err, files) => {
+            for (let file of files) {
+              deleteFile(path.join(filePath, file)).then(() => console.log(`${file} deleted`));
+            }
+          })
+          resolve();
+        } else {
+          throw err;
+        }
+      } else {
+        console.log("files-copy directory created");
+        resolve();
+      }
+    })
+  })
+}
+
+const inputPath = path.join(__dirname, "files");
+const outputPath = path.join(__dirname, "files-copy");
+
+
+makeDirectory(outputPath).then(() => {
+  fs.readdir(inputPath, (err, files) => {
+    if (err) throw err;
+    files.forEach(fileName => {
+      let inputFilePath = path.join(inputPath, fileName);
+      let outputFilePath = path.join(outputPath, fileName);
+      fs.copyFile(inputFilePath, outputFilePath, (err) => {
+        if (err) throw err;
+        console.log(`${fileName} copied to ${outputFilePath}`)
+      })
+    })
+  })
+});
+
 
