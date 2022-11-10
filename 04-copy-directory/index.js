eeb35946245bc0653a86a7d7ead300bc390b526b
rin <prettysunshine468@gmail.com>
2022-11-11 00:57:25 +0600

    fix 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,54 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 
-const deleteFile = (filePath) => {
-  return new Promise(resolve => {
-    fs.unlink(filePath, (err) => {
-      if(err) throw err;
-      resolve();
-    })
-  })
-}
-
-const makeDirectory = (filePath) => {
-  return new Promise(resolve => {
-    fs.mkdir(filePath,  (err) => {
-      if (err) {
-        if(err.errno === -17 || err.errno === -4075) {
-          console.log("folder already exists, deleting the old contents");
-          fs.readdir(filePath,  (err, files) => {
-            for (let file of files) {
-              deleteFile(path.join(filePath, file)).then(() => console.log(`${file} deleted`));
-            }
-          })
-          resolve();
-        } else {
-          throw err;
-        }
-      } else {
-        console.log("files-copy directory created");
-        resolve();
-      }
-    })
-  })
-}
-
 const inputPath = path.join(__dirname, "files");
 const outputPath = path.join(__dirname, "files-copy");
 
 
-makeDirectory(outputPath).then(() => {
-  fs.readdir(inputPath, (err, files) => {
-    if (err) throw err;
+fs.promises.mkdir(outputPath, {recursive: true}).then(async () => {
+  const files = await fs.promises.readdir(outputPath);
+  if(files) {
+    for (const file of files) {
+      let fileName = path.join(outputPath, file)
+      await fs.promises.unlink(fileName);
+    }
+  }
+  fs.promises.readdir(inputPath).then((files) => {
     files.forEach(fileName => {
       let inputFilePath = path.join(inputPath, fileName);
       let outputFilePath = path.join(outputPath, fileName);
-      fs.copyFile(inputFilePath, outputFilePath, (err) => {
-        if (err) throw err;
-        console.log(`${fileName} copied to ${outputFilePath}`)
-      })
+      fs.promises.copyFile(inputFilePath, outputFilePath).then(() => {
+          console.log(`${fileName} copied`);
+        }
+      )
     })
   })
-});
+})
 
 
